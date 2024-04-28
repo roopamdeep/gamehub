3105b538014bcc09108252162160f89147151550
roopamdeepk <roopam_kaur@yahoo.in>
2024-04-28 17:02:51 -0400

    Build a Navbar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,15 @@
+import { HStack, Image, Text } from '@chakra-ui/react';
+import React from 'react';
+import logo from '../assets/logo.webp';
+import { px } from 'framer-motion';
+
+const NavBar = () => {
+  return (
+    <HStack>
+      <Image src={logo} boxSize="60px"></Image>
+      <Text>Navbar</Text>
+    </HStack>
+  );
+};
+
+export default NavBar;
