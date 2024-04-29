b346be4ddac9a936981a50c46f36528fd0774e31
roopamdeepk <roopam_kaur@yahoo.in>
2024-04-28 22:35:41 -0400

    Building Game Cards

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -7,9 +7,10 @@ interface FetchGamesResponse {
   results: Game[];
 }
 
-interface Game {
+export interface Game {
   id: number;
   name: string;
+  background_image: string;
 }
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
